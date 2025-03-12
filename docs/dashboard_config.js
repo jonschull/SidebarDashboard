cc213e7315b6aaf9611a092f4e01eaf4d495e02d
jonschull <Jschull@gmail.com>
2025-03-12 16:37:06 -0400

    Publishing dashboard test_new_dashboard: Wed Mar 12 16:37:06 EDT 2025

---
+++ docs/dashboard_config.js
@@ -0,0 +1,14 @@
+/**
+ * Dashboard Configuration
+ * This file contains configuration settings for the current dashboard
+ */
+
+// Global dashboard configuration object
+window.DASHBOARD_CONFIG = {
+    name: 'test_new_dashboard',
+    githubUsername: 'jonschull',
+    githubRepo: 'test_new_dashboard',
+    githubPagesUrl: 'https://jonschull.github.io/test_new_dashboard/',
+    githubStatusUrl: 'https://github.com/jonschull/test_new_dashboard/actions',
+    publishStatus: 'unpublished' // Options: 'published', 'unpublished', 'test'
+};
