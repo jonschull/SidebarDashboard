7cf0a2fab925dae3631a692a9bc8f9a2d8980ea8
jonschull <Jschull@gmail.com>
2025-03-12 17:11:46 -0400

    Update GitHub Pages content for publish_test - 2025-03-12 17:11:46

---
+++ docs/publish_test/dashboard_config.js
@@ -0,0 +1,14 @@
+/**
+ * Dashboard Configuration
+ * This file contains configuration settings for the current dashboard
+ */
+
+// Global dashboard configuration object
+window.DASHBOARD_CONFIG = {
+    name: 'publish_test',
+    githubUsername: 'jonschull',
+    githubRepo: 'SidebarDashboard',
+    githubPagesUrl: 'https://jonschull.github.io/SidebarDashboard/publish_test/',
+    githubStatusUrl: 'https://github.com/jonschull/SidebarDashboard/actions',
+    publishStatus: 'unpublished' // Options: 'published', 'unpublished', 'test'
+};
